5da51b2afab8fc74f08544652f4165cb4ba4570f
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2018-10-31 15:07:04 -0700

    Update toast_snippet.java

--- toast_snippet.java
+++ toast_snippet.java
@@ -1,2 +1,4 @@
+// usually within the body of onSubmit() when moving to another activity upon 
+// clicking some button. 
 Toast t = Toast.makeText(MainActivity.this,"moved to next page",Toast.LENGTH_SHORT);
 t.show();
